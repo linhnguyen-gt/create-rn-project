215df1d1444d717dda9e47270f5f76fabb5b8232
Linh <linh.nguyen96114@gmail.com>
2025-04-25 13:46:24 +0700

    update format and create new project

---
+++ .prettierrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+    bracketSameLine: true,
+    bracketSpacing: true,
+    singleQuote: false,
+    trailingComma: "none",
+    semi: true,
+    tabWidth: 4,
+    printWidth: 120,
+    arrowParens: "always",
+    endOfLine: "lf",
+    embeddedLanguageFormatting: "auto",
+    quoteProps: "as-needed"
+};
